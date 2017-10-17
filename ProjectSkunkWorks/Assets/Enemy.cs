24dd935280459a8f86ef2e48d41f3265e66ba93e
Juriel Garcia <jurielgarcia2010@gmail.com>
2017-10-16 22:39:00 -0400

    done for the day

---
+++ ProjectSkunkWorks/Assets/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    private NavMeshAgent enemy;
+
+	// Use this for initialization
+	void Start () {
+        enemy = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
