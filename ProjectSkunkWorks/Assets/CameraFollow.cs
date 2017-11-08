7c5d69b9a436b46adc8023284cd94c0a607b23f9
Juriel Garcia <jurielgarcia2010@gmail.com>
2017-11-08 15:28:23 -0500

    Progress1
    Playing around, getting the lighting in place and making a proper scene to scale. Added some of the already prebuilt assests unity offers, just so I didnt waste time.

---
+++ ProjectSkunkWorks/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 targetPos = target.position + offset;
+            Vector3 smoothedPos = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
+            transform.position = smoothedPos;
+
+            transform.LookAt(target);
+        }
+    }
+}
